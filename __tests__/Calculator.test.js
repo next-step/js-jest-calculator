aabf898ccdd8eb9b31dc7387bc64187960fc4906
sangwon <sangwon@sangwonui-MacBookPro.local>
2023-07-20 00:30:47 +0900

    FEAT: pressOperator 및 showResult 추가

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -25,26 +25,22 @@ describe("Calculator", () => {
   test("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
     const calculator = new Calculator();
 
-    calculator.divide(3, 2);
+    calculator.divide(1, 2);
 
-    expect(calculator.result).toBe(1);
+    expect(calculator.result).toBe(0.5);
   });
   test("숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.", () => {
     const calculator = new Calculator();
 
-    calculator.insertNumber(1);
-    calculator.insertNumber(1);
-    calculator.insertNumber(1);
+    [1, 2, 3].forEach((aNumber) => calculator.pressNumber(aNumber));
 
-    expect(() => calculator.insertNumber(1)).toThrow(
-      "숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다"
-    );
+    expect(() => calculator.pressNumber(1)).toThrow();
   });
   test("계산 결과를 표현할 때 소수점 이하는 버림한다.", () => {
     const calculator = new Calculator();
 
     calculator.result = 3.44444;
 
-    expect(calculator.result).toBe(3);
+    expect(calculator.showResult()).toBe(3);
   });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,10 +2,10 @@ export class Calculator {
   a = null;
   b = null;
   operator = null;
-  #result = null;
+  result = null;
 
   constructor() {}
-  
+
   add(a, b) {
     this.#checkNumber(a, b);
     this.result = a + b;
@@ -23,12 +23,16 @@ export class Calculator {
     this.result = a / b;
   }
 
-  insertNumber(aNumber) {
+  pressNumber(aNumber) {
+    if (!DIGITS.includes(aNumber)) {
+      throw Error(ERROR_MESSAGES.PRESS_ONLY_NUMBER);
+    }
+
     let existNumber = this.operator ? this.b : this.a;
     existNumber = (existNumber ?? "") + String(aNumber);
 
     if (existNumber.length > 3) {
-      throw Error("숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다");
+      throw Error(ERROR_MESSAGES.EXCEED_MAXIMUM_INPUT_NUMBERS);
     }
 
     if (this.operator) {
@@ -38,20 +42,35 @@ export class Calculator {
     }
   }
 
-  get result() {
-    return this.#result;
+  pressOperator(aOperator) {
+    if (!["*", "/", "+", "%", "-"].includes(aOperator)) {
+      throw Error(ERROR_MESSAGES.PRESS_ONLY_OPERATOR);
+    }
+    this.operator = aOperator;
   }
-  set result(aNumber) {
-    this.#result = Math.floor(aNumber);
+
+  showResult() {
+    return Math.floor(this.result);
   }
 
   #checkNumber(...numbers) {
     for (let aNumber of numbers) {
       if (typeof aNumber !== "number") {
-        throw Error("Not type Number");
+        throw Error(ERROR_MESSAGES.NOT_TYPE_NUMBER);
       }
     }
 
     return numbers;
   }
 }
+
+const ERROR_MESSAGES = {
+  EXCEED_MAXIMUM_INPUT_NUMBERS:
+    "숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다.",
+  PRESS_ONLY_NUMBER: "숫자를 입력해주세요.",
+  PRESS_ONLY_OPERATOR: "연산자를 입력해주세요.",
+};
+
+const DIGITS = Array(10)
+  .fill(0)
+  .map((v, i) => v + i);

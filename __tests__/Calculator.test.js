5eaa3b81c3aa9169131aced542245ff19d051a90
손진영 <sm099@naver.com>
2023-07-20 00:46:50 +0900

    test: 2개의 숫자를 더하는 기능 테스트 진행

---
+++ __tests__/Calculator.test.js
@@ -0,0 +1,8 @@
+import Calulator from '../src/Calulator';
+
+describe('계산기 기능 요구사항 테스트', () => {
+  test('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    const result = Calulator.add(1, 2);
+    expect(result).toBe(3);
+  });
+});

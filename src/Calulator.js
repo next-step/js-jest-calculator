352436a873bad4e02b308d9f59e9d84cec5f99e4
손진영 <sm099@naver.com>
2023-07-20 01:11:09 +0900

    feat: 숫자는 최대 3자리 수까지만 계산하기 위한 예외 처리 구현

--- src/Calulator.js
+++ src/Calulator.js
@@ -1,25 +1,29 @@
 import MESSAGE from './constants/Message';
-import { isCalculateError } from './utils';
+import { isCalculateError, isOrLessThanThreeDigits } from './utils';
 
 class Calulator {
   static add(...args) {
     const [a, b] = args;
     if (isCalculateError(args)) return MESSAGE.ARG_ERROR;
+    if (isOrLessThanThreeDigits(a, b)) return MESSAGE.DIGIT_ERROR;
     return a + b;
   }
   static subtract(...args) {
     const [a, b] = args;
     if (isCalculateError(args)) return MESSAGE.ARG_ERROR;
+    if (isOrLessThanThreeDigits(a, b)) return MESSAGE.DIGIT_ERROR;
     return a - b;
   }
   static multiply(...args) {
     const [a, b] = args;
     if (isCalculateError(args)) return MESSAGE.ARG_ERROR;
+    if (isOrLessThanThreeDigits(a, b)) return MESSAGE.DIGIT_ERROR;
     return a * b;
   }
   static divide(...args) {
     const [a, b] = args;
     if (isCalculateError(args)) return MESSAGE.ARG_ERROR;
+    if (isOrLessThanThreeDigits(a, b)) return MESSAGE.DIGIT_ERROR;
     return a / b;
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,8 @@ export const isCalculateError = (arg) => {
   const isTwoArguments = arg.length === 2;
   return Validator.isError(!isTwoArguments, MESSAGE.ARG_ERROR);
 };
+
+export const isOrLessThanThreeDigits = (a, b) => {
+  const isOrLessThanThreeDigits = [a, b].some((number) => number > 999);
+  return Validator.isError(isOrLessThanThreeDigits, MESSAGE.DIGIT_ERROR);
+};

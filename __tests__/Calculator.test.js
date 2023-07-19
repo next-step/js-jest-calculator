46100cbd5c9c52b95c0f1f54250ec989cd7dff3d
손진영 <sm099@naver.com>
2023-07-20 00:49:12 +0900

    test: 2개의 숫자를 빼는 기능 테스트 진행

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -5,4 +5,8 @@ describe('계산기 기능 요구사항 테스트', () => {
     const result = Calulator.add(1, 2);
     expect(result).toBe(3);
   });
+  test('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    const result = Calulator.subtract(2, 1);
+    expect(result).toBe(1);
+  });
 });

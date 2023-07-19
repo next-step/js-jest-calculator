96da05f389c1a95ca624a80731899625254b8729
손진영 <sm099@naver.com>
2023-07-20 01:11:54 +0900

    test: 최대 3자리 수까지만 계산하기 위한 예외 처리 테스트 진행

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,4 +1,5 @@
 import Calulator from '../src/Calulator';
+import MESSAGE from '../src/constants/Message';
 
 describe('계산기 기능 요구사항 테스트', () => {
   test('2개의 숫자에 대해 덧셈이 가능하다.', () => {
@@ -16,5 +17,18 @@ describe('계산기 기능 요구사항 테스트', () => {
   test('2개의 숫자에 대해 곱셈이 가능하다.', () => {
     const result = Calulator.divide(5, 1);
     expect(result).toBe(5);
+  });
+});
+
+describe('계산기 예외 처리 테스트', () => {
+  test('숫자는 한번에 최대 4자리 수 이상이 되면 예외처리가 되어야 한다.', () => {
+    const result = [
+      Calulator.add(1000, 2),
+      Calulator.subtract(1000, 2),
+      Calulator.multiply(1000, 2),
+      Calulator.divide(1000, 2),
+    ];
+    const digitErrorArr = Array(result.length).fill(MESSAGE.DIGIT_ERROR);
+    expect(result).toStrictEqual(digitErrorArr);
   });
 });

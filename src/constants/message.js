84072e42ac41adde2acb47aa3e72befa84ddffa5
sonsurim <surim014@naver.com>
2023-07-21 02:15:35 +0900

    에러 메시지 상수화

---
+++ src/constants/message.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  NOT_ACCESS_VALUE: '값에 직접 접근할 수 없습니다!',
+  NOT_SUPPORT: '지원하지 않는 메서드입니다!',
+  INVALID_NUMBER: '숫자로만 입력이 가능합니다!',
+  INVALID_DIGIT: '숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다!',
+}

9d6632ffa65c89d5db83813943865ed6bee0f3b3
손진영 <sm099@naver.com>
2023-07-20 20:44:33 +0900

    test: 테스트 케이스 추가

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,50 +1,60 @@
 import Calculator from '../src/Calculator';
-import MESSAGE from '../src/constants/Message';
+import { CALCULATE_COMMAND } from '../src/constants/commands';
+import { TEST_MESSAGE } from '../src/constants/message';
+import {
+  MESSAGE_TEST_SAMPLES,
+  TEST_FUCNTION_SAMPLES,
+} from '../src/constants/testSample';
 
 const calculator = new Calculator();
 
+const genArithmeticExpectResults = (params, message, type) => {
+  test.each(params)(message, ({ a, b, expected }) => {
+    const calculate_table = {
+      ADD: calculator.add(a, b),
+      SUBTRACT: calculator.subtract(a, b),
+      MULTIPLY: calculator.multiply(a, b),
+      DIVIDE: calculator.divide(a, b),
+    };
+    expect(calculate_table[type]).toBe(expected);
+  });
+};
+
+const arithmeticTestResults = () => {
+  const commands = Object.values(CALCULATE_COMMAND);
+  TEST_FUCNTION_SAMPLES.forEach((params, i) => {
+    genArithmeticExpectResults(
+      params,
+      TEST_MESSAGE.FUNCTIONAL_TEST[commands[i]],
+      CALCULATE_COMMAND[commands[i]]
+    );
+  });
+};
+
+const genMessageResults = (params, message) => {
+  test.each(params)(message, ({ args, expected }) => {
+    const calculate_table = [
+      calculator.add(...args),
+      calculator.subtract(...args),
+      calculator.multiply(...args),
+      calculator.divide(...args),
+    ];
+    calculate_table.forEach((received) => {
+      expect(received).toBe(expected);
+    });
+  });
+};
+
+const confirmMessageTestResults = () => {
+  MESSAGE_TEST_SAMPLES.forEach((params, i) => {
+    genMessageResults(params, TEST_MESSAGE.MESSAGE_TEST[i]);
+  });
+};
+
 describe('계산기 기능 요구사항 테스트', () => {
-  test('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    const result = calculator.add(1, 2);
-    expect(result).toBe(3);
-  });
-  test('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    const result = calculator.subtract(2, 1);
-    expect(result).toBe(1);
-  });
-  test('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    const result = calculator.multiply(3, 1);
-    expect(result).toBe(3);
-  });
-  test('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    const result = calculator.divide(5, 1);
-    expect(result).toBe(5);
-  });
-  test('계산 결과를 표현할 때 소수점 이하는 버림할 수 있어야 한다.', () => {
-    const result = calculator.divide(1, 5);
-    expect(result).toBe(0);
-  });
+  arithmeticTestResults();
 });
 
 describe('계산기 메시지 확인 테스트', () => {
-  test('숫자는 한번에 최대 4자리 수 이상이 되면 메시지 확인이 되어야 한다.', () => {
-    const result = [
-      calculator.add(1000, 2),
-      calculator.subtract(1000, 2),
-      calculator.multiply(1000, 2),
-      calculator.divide(1000, 2),
-    ];
-    const digitErrorArr = Array(result.length).fill(MESSAGE.DIGIT_ERROR);
-    expect(result).toStrictEqual(digitErrorArr);
-  });
-  test('만약 2개의 숫자를 계산하지 않는 경우 메시지 확인이 되어야 한다.', () => {
-    const result = [
-      calculator.add(1, 2, 3),
-      calculator.subtract(3),
-      calculator.multiply(5, 2, 1, 7, 3),
-      calculator.divide(6, 2, 3, 4),
-    ];
-    const argErrorArr = Array(result.length).fill(MESSAGE.ARG_ERROR);
-    expect(result).toStrictEqual(argErrorArr);
-  });
+  confirmMessageTestResults();
 });

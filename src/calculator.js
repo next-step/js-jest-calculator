140810f03d945c33f45ad8f844800ae91dc8fb95
Jason <jasondal91@gmail.com>
2023-07-20 18:23:33 +0900

    feat: 숫자 3자리 이상 입력 체크

--- src/calculator.js
+++ src/calculator.js
@@ -1,17 +1,31 @@
 export default class Calculator {
   sum(num1, num2) {
+    this.validateOverThreeDigits([num1, num2]);
     return num1 + num2;
   }
 
   subtract(num1, num2) {
+    this.validateOverThreeDigits([num1, num2]);
     return num1 - num2;
   }
 
   multiply(num1, num2) {
+    this.validateOverThreeDigits([num1, num2]);
     return num1 * num2;
   }
 
   divide(num1, num2) {
+    this.validateOverThreeDigits([num1, num2]);
     return num1 / num2;
+  }
+
+  validateOverThreeDigits(numbers) {
+    const haveOverThreeDigits = numbers.some(
+      (number) => String(number).length > 3
+    );
+
+    if (haveOverThreeDigits) {
+      throw new Error('숫자는 한 번에 3자리까지 입력 가능합니다.');
+    }
   }
 }

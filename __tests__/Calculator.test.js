8f30a77c8c2904e1530b8a343158ad2ce7265976
손진영 <sm099@naver.com>
2023-07-20 20:42:36 +0900

    style(test): 예외 처리 테스트에서 메시지 확인 테스트로 변경

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -26,8 +26,8 @@ describe('계산기 기능 요구사항 테스트', () => {
   });
 });
 
-describe('계산기 예외 처리 테스트', () => {
-  test('숫자는 한번에 최대 4자리 수 이상이 되면 예외처리가 되어야 한다.', () => {
+describe('계산기 메시지 확인 테스트', () => {
+  test('숫자는 한번에 최대 4자리 수 이상이 되면 메시지 확인이 되어야 한다.', () => {
     const result = [
       calculator.add(1000, 2),
       calculator.subtract(1000, 2),
@@ -37,7 +37,7 @@ describe('계산기 예외 처리 테스트', () => {
     const digitErrorArr = Array(result.length).fill(MESSAGE.DIGIT_ERROR);
     expect(result).toStrictEqual(digitErrorArr);
   });
-  test('만약 2개의 숫자를 계산하지 않는 경우 예외 처리가 되어야 한다.', () => {
+  test('만약 2개의 숫자를 계산하지 않는 경우 메시지 확인이 되어야 한다.', () => {
     const result = [
       calculator.add(1, 2, 3),
       calculator.subtract(3),

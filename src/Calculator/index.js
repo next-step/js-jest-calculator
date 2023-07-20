2d8561a2f95d2f38f7e104dbf3344c8a7025a61e
Hoseok Na <hsna7024@gmail.com>
2023-07-20 22:09:26 +0900

    feat: 덧셈 다루는 숫자가 최대 3자리가 넘어갈 때 에러 발생하도록 구현

--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -1,5 +1,12 @@
 class Calculator {
   sum(a, b) {
+    if (a > 999) {
+      throw new Error('숫자 범위가 3자리를 넘을 수 없습니다.')
+    }
+    if (b > 999) {
+      throw new Error('숫자 범위가 3자리를 넘을 수 없습니다.')
+    }
+
     return a + b
   }
 

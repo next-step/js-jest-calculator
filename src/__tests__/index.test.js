608798c400fa5987f2db350dbcfb03ef4355ba67
gusrb3164 <gusrb3164@gmail.com>
2023-07-20 23:31:03 +0900

    feat: 최대 3자리 수까지만 다룰 수 있는 요구사항 추가

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -22,4 +22,32 @@ describe('계산기 미션', () => {
   test('0으로는 나눌 수 없다.', () => {
     expect(() => calculator.divide(5, 0)).toThrowError('0으로 나눌 수 없습니다.');
   });
+
+  test.each([
+    [1000, 1],
+    [1, 1000],
+    [-999, 1000],
+    [-1000, 999],
+    [10000, 10],
+  ])('3자리가 넘는 수는 다룰 수 없다.', (num1, num2) => {
+    const errorMessage = '숫자는 한번에 최대 3자리의 수까지만 다룰 수 있습니다.';
+
+    expect(() => calculator.sum(num1, num2)).toThrowError(errorMessage);
+    expect(() => calculator.subtract(num1, num2)).toThrowError(errorMessage);
+    expect(() => calculator.multiply(num1, num2)).toThrowError(errorMessage);
+    expect(() => calculator.divide(num1, num2)).toThrowError(errorMessage);
+  });
+
+  test.each([
+    [999, 999],
+    [-999, -999],
+    [0, 100],
+    [999, -999],
+    [-999, 999],
+  ])('3자리 이하의 수는 다룰 수 있다.', (num1, num2) => {
+    expect(calculator.sum(num1, num2)).toBe(num1 + num2);
+    expect(calculator.subtract(num1, num2)).toBe(num1 - num2);
+    expect(calculator.multiply(num1, num2)).toBe(num1 * num2);
+    expect(calculator.divide(num1, num2)).toBe(num1 / num2);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 export class Calculator {
   sum(num1, num2) {
+    this._validateMaximumInput(num1, num2);
+
     return num1 + num2;
   }
 
   subtract(num1, num2) {
+    this._validateMaximumInput(num1, num2);
+
     return num1 - num2;
   }
 
   multiply(num1, num2) {
+    this._validateMaximumInput(num1, num2);
+
     return num1 * num2;
   }
 
@@ -16,6 +22,14 @@ export class Calculator {
       throw new Error('0으로 나눌 수 없습니다.');
     }
 
+    this._validateMaximumInput(num1, num2);
+
     return num1 / num2;
+  }
+
+  _validateMaximumInput(num1, num2) {
+    if (Math.abs(num1) >= 1000 || Math.abs(num2) >= 1000) {
+      throw new Error('숫자는 한번에 최대 3자리의 수까지만 다룰 수 있습니다.');
+    }
   }
 }

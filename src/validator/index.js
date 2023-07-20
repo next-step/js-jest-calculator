69ba517a3a1012df296f5c7c70a291ab8169db24
손진영 <sm099@naver.com>
2023-07-20 21:02:36 +0900

    fix(Validator): isError 메서드가 boolean 값이 아닌 에러 메시지를 반환하도록 변경

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,11 +1,6 @@
 class Validator {
   static isError(errorCondition, errorMessage) {
-    try {
-      if (errorCondition) throw new Error(errorMessage);
-      return false;
-    } catch (errorMessage) {
-      return true;
-    }
+    if (errorCondition) return errorMessage;
   }
 }
 

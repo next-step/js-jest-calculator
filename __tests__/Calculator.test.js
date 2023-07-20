d738d4b99a8054f4b63a259dc08d04383cf29310
Hoseok Na <hsna7024@gmail.com>
2023-07-20 21:50:14 +0900

    test: 계산기 생성 부분 최상단에 한번만 하도록 리팩토링

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,33 +1,27 @@
 import Calculator from '../src/Calculator'
 
+const calculator = new Calculator()
+
 describe('계산기 덧셈 테스트', () => {
   test('sum(1,1) 의 결과값은 2다.', () => {
-    const calculator = new Calculator()
-
     expect(calculator.sum(1, 1)).toBe(2)
   })
 })
 
 describe('계산기 뺄셈 테스트', () => {
   test('subtract(1,1) 의 결과값은 0이다.', () => {
-    const calculator = new Calculator()
-
     expect(calculator.substract(1, 1)).toBe(0)
   })
 })
 
 describe('계산기 곱셈 테스트', () => {
   test('multiply(2,2) 의 결과값은 4다.', () => {
-    const calculator = new Calculator()
-
     expect(calculator.multiply(2, 2)).toBe(4)
   })
 })
 
 describe('계산기 나눗셈 테스트', () => {
   test('divide(2,2) 의 결과값은 1이다.', () => {
-    const calculator = new Calculator()
-
     expect(calculator.divide(2, 2)).toBe(1)
   })
 })

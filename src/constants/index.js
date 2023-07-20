bdbae51fb1cec6c1527cd0c88652a59afe8f021e
손진영 <sm099@naver.com>
2023-07-20 21:11:17 +0900

    refactor(utils): isOrLessThanThreeDigits 함수 내 상수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const MAX_ARGUMENT_LENGTH = 2;
+export const MAX_DIGIT_NUMBER = 999;
+export const ZERO = 0;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,18 @@
+import { MAX_ARGUMENT_LENGTH, MAX_DIGIT_NUMBER, ZERO } from '../constants';
 import ERROR_MESSAGE from '../constants/message';
 import Validator from '../validator';
 
 export const isCalculateError = (arg) => {
-  const isTwoArguments = arg.length === 2;
+  const isTwoArguments = arg.length === MAX_ARGUMENT_LENGTH;
   return Validator.isError(!isTwoArguments, ERROR_MESSAGE.ARG_ERROR);
 };
 
 export const isOrLessThanThreeDigits = (args) => {
-  const isOrLessThanThreeDigits = args.some((number) => number > 999);
+  const isOrLessThanThreeDigits = args.some(
+    (number) => number > MAX_DIGIT_NUMBER
+  );
   return Validator.isError(isOrLessThanThreeDigits, ERROR_MESSAGE.DIGIT_ERROR);
 };
 
 export const genFloorValue = (value) =>
-  value < 0 ? Math.ceil(value) : Math.floor(value);
+  value < ZERO ? Math.ceil(value) : Math.floor(value);

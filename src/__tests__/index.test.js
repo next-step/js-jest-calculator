45817315063b20e205988f010871af90861c051d
gusrb3164 <gusrb3164@gmail.com>
2023-07-20 23:03:44 +0900

    feat: 나눗셈 요구사항 추가

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -14,4 +14,12 @@ describe('계산기 미션', () => {
   test('2개의 숫자에 대해 곱셈이 가능하다.', () => {
     expect(calculator.multiply(2, 4)).toBe(8);
   });
+
+  test('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    expect(calculator.divide(8, 4)).toBe(2);
+  });
+
+  test('0으로는 나눌 수 없다.', () => {
+    expect(() => calculator.divide(5, 0)).toThrowError('0으로 나눌 수 없습니다.');
+  });
 });
--- src/index.js
+++ src/index.js
@@ -10,4 +10,12 @@ export class Calculator {
   multiply(num1, num2) {
     return num1 * num2;
   }
+
+  divide(num1, num2) {
+    if (num2 === 0) {
+      throw new Error('0으로 나눌 수 없습니다.');
+    }
+
+    return num1 / num2;
+  }
 }

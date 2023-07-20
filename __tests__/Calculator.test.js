e3c5e9f8491c2d3bdcfd596f60d13cce6e59f528
Hoseok Na <hsna7024@gmail.com>
2023-07-20 21:30:22 +0900

    test: 뺄셈 substract 함수 테스트 코드 작성

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -7,3 +7,11 @@ describe('계산기 덧셈 테스트', () => {
     expect(calculator.sum(1, 1)).toBe(2)
   })
 })
+
+describe('계산기 뺄셈 테스트', () => {
+  test('subtract(1,1) 의 결과값은 0이다.', () => {
+    const calculator = new Calculator()
+
+    expect(calculator.substract(1, 1)).toBe(0)
+  })
+})

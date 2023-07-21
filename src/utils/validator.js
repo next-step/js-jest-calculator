827c5c7422ee0c0c09f3cafe73c6b30642e43235
sonsurim <surim014@naver.com>
2023-07-21 13:53:19 +0900

    isNumber validate 조건 추가

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,7 +1,7 @@
 import { ERROR_MESSAGE } from '../constants/message'
 import { floor } from './math'
 
-export const isNumber = (num) => Object.prototype.toString.call(num) === '[object Number]'
+export const isNumber = (num) => !isNaN(num) && Object.prototype.toString.call(num) === '[object Number]'
 
 export const isValidNumbers = (...numbers) => {
   const isValid = numbers.every(number => isNumber(number))

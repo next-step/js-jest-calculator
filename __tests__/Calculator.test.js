3a1c495b8bd9be8bfd0f7aabdd30cedb41e634c2
kimhaneui <139193311+TraportStaffs@users.noreply.github.com>
2023-07-21 09:37:01 +0900

    테스트 코드 추가 및 마크다운으로 요구사항 이동

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,21 +1,44 @@
 const { add, subtract, multiply, divide } = require("../src/index");
 
 describe('add', () => {
-  test('adds', () => {
+  test('adds case1', () => {
     expect(add(1, 2)).toBe(3);
   });
-    test("subtract", () => {
-    expect(subtract(5, 2)).toBe(3);
-  });
-
-  test("multiplies", () => {
-    expect(multiply(3, 4)).toBe(12);
-  });
-
-  test("divides", () => {
-    expect(divide(10, 2)).toBe(5);
+  test('adds case2', () => {
+    expect(add(-5, -3)).toBe(-8);
   });
 });
+describe('subtract', () => {
+  test("subtract case1", () => {
+    expect(subtract(5, 2)).toBe(3);
+  });
+  test('subtract case2', () => {
+    expect(subtract(-5, -2)).toBe(-3);
+  });
+})
+describe('multiplies', () => {
+  test("multiplies case1", () => {
+    expect(subtract(5, 2)).toBe(3);
+  });
+  test('multiplies case2', () => {
+    expect(multiply(5, 0)).toBe(0);
+  });
+})
+describe('divides case1', () => {
+  test("divides", () => {
+    expect(subtract(5, 2)).toBe(3);
+  });
+  test("divides case1", () => {
+    expect(divide(10, 2)).toBe(5);
+  });
 
-
+  test("divides case2", () => {
+    expect(divide(10, 1)).toBe(10);
+  });
+  test("divides case3 by 0", () => {
+    expect(() => {
+      divide(10, 0);
+    }).toThrow(Error("Cannot divide by 0."));
+  });
+})
 

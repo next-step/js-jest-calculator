68061994a51767605a9de4b0a92213f14c13a5b7
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-22 03:11:34 +0900

    ♻️ refactor: 유효성 검사 로직 개선

--- src/utils/validateCalculator.js
+++ src/utils/validateCalculator.js
@@ -1,33 +1,24 @@
 import { ERROR_MESSAGE } from '../constants/errorMessage.js';
 import { CALCULATOR_CONFIGURE } from '../constants/calculatorConfigure.js';
 
-export const validateCalculator = (first, second) => {
+const isValueInRange = (operands, max, min) => operands.every((operand) => operand < max && operand > min);
+const isValidNumber = (operands) => operands.every((operand) => typeof operand === 'number' && !Number.isNaN(operand));
+
+export const validateCalculator = (...operands) => {
   const { NOT_NUMBER, OVER_MAX_LENGTH, REQUIRED_NUMBER } = ERROR_MESSAGE;
-  const { MAX_NUMBER } = CALCULATOR_CONFIGURE;
+  const { MAX_NUMBER, MIN_NUMBER } = CALCULATOR_CONFIGURE;
 
-  if (!first || !second) {
-    return {
-      success: false,
-      message: REQUIRED_NUMBER
-    };
+  if (operands.length !== 2) {
+    throw REQUIRED_NUMBER;
   }
 
-  if (typeof first !== 'number' || typeof second !== 'number') {
-    return {
-      success: false,
-      message: NOT_NUMBER
-    };
+  if (!isValidNumber(operands)) {
+    throw NOT_NUMBER;
   }
 
-  if (first > MAX_NUMBER || second > MAX_NUMBER) {
-    return {
-      success: false,
-      message: OVER_MAX_LENGTH
-    };
+  if (!isValueInRange(operands, MAX_NUMBER, MIN_NUMBER)) {
+    throw OVER_MAX_LENGTH;
   }
 
-  return {
-    success: true,
-    message: 'success'
-  };
+  return true;
 };

b10f2468155b9a56b522968b7a8848752f9abe60
bxxmi <bmi6638@gmail.com>
2023-07-21 19:44:05 +0900

    [feat] 클래스 내 메소드 에러 처리 및 로직 수정

--- src/classes/CalculatorClass.js
+++ src/classes/CalculatorClass.js
@@ -1,32 +1,35 @@
-class Calculator {
-  constructor(number1, number2, calculatedNumber) {
-    this.number1 = number1;
-    this.number2 = number2;
+export default class Calculator {
+  constructor(calculatedNumber) {
     this.calculatedNumber = calculatedNumber;
   }
-  
-  addNumber() {
-    this.calculatedNumber = Math.floor(this.number1 + this.number2);
-    return validateNumberLength(this.calculatedNumber);
+
+  addNumber(number1, number2) {
+    this.calculatedNumber = Math.floor(number1 + number2);
+    return this.validateNumberLength(this.calculatedNumber);
   }
 
-  substractNumber() {
-    this.calculatedNumber = Math.floor(this.number1 - this.number2);
-    return validateNumberLength(this.calculatedNumber);
+  substractNumber(number1, number2) {
+    this.calculatedNumber = Math.floor(number1 - number2);
+    return this.validateNumberLength(this.calculatedNumber);
   }
 
-  multiplyNumber() {
-    this.calculatedNumber = Math.floor(this.number1 * this.number2);
-    return validateNumberLength(this.calculatedNumber);
+  multiplyNumber(number1, number2) {
+    this.calculatedNumber = Math.floor(number1 * number2);
+    return this.validateNumberLength(this.calculatedNumber);
   }
 
-  divideNumber() {
-    this.calculatedNumber = Math.floor(this.number1 / this.number2);
-    return validateNumberLength(this.calculatedNumber);
+  divideNumber(number1, number2) {
+    if (number2 === 0) throw new Error("0으로는 나눌 수 없습니다.");
+
+    this.calculatedNumber = Math.floor(number1 / number2);
+    return this.validateNumberLength(this.calculatedNumber);
   }
 
   validateNumberLength(number) {
-    if (number.toString().length > 3) return false;
+    if (number.toString().length > 3) {
+      throw new Error("계산은 최대 3자리 수까지 가능합니다.");
+    }
+
     return number;
   }
 }

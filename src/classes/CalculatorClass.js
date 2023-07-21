443cd89b15f4cf8888078f34792236a730f0a383
bxxmi <bmi6638@gmail.com>
2023-07-21 23:12:18 +0900

    [feat] 클래스 내부 로직 변경

--- src/classes/CalculatorClass.js
+++ src/classes/CalculatorClass.js
@@ -1,35 +1,52 @@
 export default class Calculator {
-  constructor(calculatedNumber) {
+  constructor(calculatedNumber, isValidNumber) {
     this.calculatedNumber = calculatedNumber;
+    this.isValidNumber = isValidNumber;
   }
 
   addNumber(number1, number2) {
-    this.calculatedNumber = Math.floor(number1 + number2);
-    return this.validateNumberLength(this.calculatedNumber);
+    this.isValidNumber = this.validateNumberLength(number1, number2);
+
+    if (this.isValidNumber) {
+      this.calculatedNumber = Math.floor(number1 + number2);
+      return this.calculatedNumber;
+    }
   }
 
   substractNumber(number1, number2) {
-    this.calculatedNumber = Math.floor(number1 - number2);
-    return this.validateNumberLength(this.calculatedNumber);
+    this.isValidNumber = this.validateNumberLength(number1, number2);
+
+    if (this.isValidNumber) {
+      this.calculatedNumber = Math.floor(number1 - number2);
+      return this.calculatedNumber;
+    }
   }
 
   multiplyNumber(number1, number2) {
-    this.calculatedNumber = Math.floor(number1 * number2);
-    return this.validateNumberLength(this.calculatedNumber);
+    this.isValidNumber = this.validateNumberLength(number1, number2);
+
+    if (this.isValidNumber) {
+      this.calculatedNumber = Math.floor(number1 * number2);
+      return this.calculatedNumber;
+    }
   }
 
   divideNumber(number1, number2) {
     if (number2 === 0) throw new Error("0으로는 나눌 수 없습니다.");
 
-    this.calculatedNumber = Math.floor(number1 / number2);
-    return this.validateNumberLength(this.calculatedNumber);
+    this.isValidNumber = this.validateNumberLength(number1, number2);
+
+    if (this.isValidNumber) {
+      this.calculatedNumber = Math.floor(number1 / number2);
+      return this.calculatedNumber;
+    }
   }
 
-  validateNumberLength(number) {
-    if (number.toString().length > 3) {
-      throw new Error("계산은 최대 3자리 수까지 가능합니다.");
+  validateNumberLength(number1, number2) {
+    if (number1.toString().length > 3 || number2.toString().length > 3) {
+      throw new Error("숫자 입력은 최대 3자리 수까지 가능합니다.");
     }
 
-    return number;
+    return true;
   }
 }

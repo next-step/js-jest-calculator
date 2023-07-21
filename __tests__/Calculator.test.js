0222668b0002088e08c70fe4dca84ffa6c2aee5b
ericagong <48196721+ericagong@users.noreply.github.com>
2023-07-21 19:28:19 +0900

    feat: [feature2] 유효한 피연산자 2개와 연산자 1개를 기반으로 연산 결과를 리턴하는 코드 구현

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,8 +1,8 @@
 import Calculator from "../src";
 
 describe("피연산자 두 개와 연산자 하나의 연산 결과를 반환한다.", () => {
-  const operand1 = -1;
-  const operand2 = 30;
+  const pos_operand = 1;
+  const neg_operand = -31;
 
   const pos_zero = +0;
   const neg_zero = -0;
@@ -10,24 +10,34 @@ describe("피연산자 두 개와 연산자 하나의 연산 결과를 반환한
   const calculator = new Calculator();
 
   it("두 개의 피연산자에 대해 덧셈 결과를 반환한다.", () => {
-    expect(calculator.add(operand1, operand2)).toBe(operand1 + operand2);
+    expect(calculator.add(pos_operand, neg_operand)).toBe(
+      pos_operand + neg_operand
+    );
   });
 
   it("두 개의 피연산자에 대해 뺄셈 결과를 반환한다.", () => {
-    expect(calculator.subtarct(operand1, operand2)).toBe(operand1 - operand2);
+    expect(calculator.subtarct(pos_operand, neg_operand)).toBe(
+      pos_operand - neg_operand
+    );
   });
 
   it("두 개의 피연산자에 대해 곱셈 결과를 반환한다.", () => {
-    expect(calculator.multiply(operand1, operand2)).toBe(operand1 * operand2);
+    expect(calculator.multiply(pos_operand, neg_operand)).toBe(
+      pos_operand * neg_operand
+    );
   });
 
   it("두 개의 피연산자에 대해 나눗셈 결과를 반환한다.", () => {
-    expect(calculator.divide(operand1, operand2)).toBe(operand1 / operand2);
+    expect(calculator.divide(pos_operand, neg_operand)).toBe(
+      pos_operand / neg_operand
+    );
   });
 
-  it("첫 번째 피연산자는 0이 아니고, 두 번쨰 피연산자가 +0/-0인 경우, 나눗셈 결과 Infinity/-Infinity를 반환한다.", () => {
-    expect(calculator.divide(operand1, pos_zero)).toBe(Infinity);
-    expect(calculator.divide(operand1, neg_zero)).toBe(-Infinity);
+  it("첫 번째 피연산자는 0이 아니고, 두 번쨰 피연산자가 +0/-0인 경우, 나눗셈 결과 (첫번째 피연산자 부호 * 두 번째 피연산자 부호) * Infinity를 반환한다.", () => {
+    expect(calculator.divide(pos_operand, pos_zero)).toBe(Infinity);
+    expect(calculator.divide(pos_operand, neg_zero)).toBe(-Infinity);
+    expect(calculator.divide(neg_operand, pos_zero)).toBe(-Infinity);
+    expect(calculator.divide(neg_operand, neg_zero)).toBe(Infinity);
   });
 
   it("두 피연산자가 모두 0일 때, 나눗셈 결과 NaN을 반환한다.", () => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 export default class Calculator {
-  add(operand1, operand2) {}
+  add(operand1, operand2) {
+    return operand1 + operand2;
+  }
 
-  subtarct(operand1, operand2) {}
+  subtarct(operand1, operand2) {
+    return operand1 - operand2;
+  }
 
-  multiply(operand1, operand2) {}
+  multiply(operand1, operand2) {
+    return operand1 * operand2;
+  }
 
-  divide(operand1, operand2) {}
+  divide(operand1, operand2) {
+    return operand1 / operand2;
+  }
 }

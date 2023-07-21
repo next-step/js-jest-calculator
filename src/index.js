39b7f60fe1e7dc48bc12fc41e88c74f163d235d5
max.cha <38166372+Coreight98@users.noreply.github.com>
2023-07-22 00:07:52 +0900

    feat: 테스트 코드 작성 (#13)

---
+++ src/index.js
@@ -0,0 +1,34 @@
+class Calculator {
+  valid(...nums) {
+    nums.forEach((num) => {
+      if (num.toString().length > 3) {
+        throw new Error("숫자는 최대 3자리까지만 작성 가능하다.");
+      }
+    });
+  }
+  sum(a, b) {
+    this.valid(a, b);
+
+    return a + b;
+  }
+  subtract(a, b) {
+    this.valid(a, b);
+
+    return a - b;
+  }
+  multiply(a, b) {
+    this.valid(a, b);
+
+    return a * b;
+  }
+  divide(a, b) {
+    this.valid(a, b);
+
+    if (b === 0) {
+      throw new Error("b는 0이 될 수 없습니다.");
+    }
+    return Math.trunc(a / b);
+  }
+}
+
+export default Calculator;

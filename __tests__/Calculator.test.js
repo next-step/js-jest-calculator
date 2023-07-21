9f398498bf1b3676d8cd40bd3d6873788b5b6599
Hoseok Na <hsna7024@gmail.com>
2023-07-21 21:48:52 +0900

    test: 덧셈 소수점 버리기 테스트 작성

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -7,6 +7,9 @@ describe('계산기 덧셈 테스트', () => {
   test('sum(1,1) 의 결과값은 2다.', () => {
     expect(calculator.sum(1, 1)).toBe(2)
   })
+  test('sum(1.5, 1) 의 결과값은 2다.', () => {
+    expect(calculator.sum(1.5, 1)).toBe(2)
+  })
 })
 
 describe('계산기 뺄셈 테스트', () => {

597bf19319a6936899da0152149a8ba75e716110
Jason <jasondal91@gmail.com>
2023-07-21 19:38:39 +0900

    feat: roundDown util 적용

--- src/operations.js
+++ src/operations.js
@@ -1,23 +1,24 @@
+import { roundDown } from './utils/number';
 import { overThreeLength } from './validation';
 
 export class BasicOperation {
   sum(...args) {
     overThreeLength(args);
-    return Math.floor(args.reduce((prev, curr) => prev + curr));
+    return roundDown(args.reduce((prev, curr) => prev + curr));
   }
 
   subtract(...args) {
     overThreeLength(args);
-    return Math.floor(args.reduce((prev, curr) => prev - curr));
+    return roundDown(args.reduce((prev, curr) => prev - curr));
   }
 
   multiply(...args) {
     overThreeLength(args);
-    return Math.floor(args.reduce((prev, curr) => prev * curr));
+    return roundDown(args.reduce((prev, curr) => prev * curr));
   }
 
   divide(...args) {
     overThreeLength(args);
-    return Math.floor(args.reduce((prev, curr) => prev / curr));
+    return roundDown(args.reduce((prev, curr) => prev / curr));
   }
 }

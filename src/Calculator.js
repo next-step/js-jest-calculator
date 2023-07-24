eae21afc3b192d0230d593804921017ed609110a
ding-co <pangoons@naver.com>
2023-07-24 11:58:56 +0900

    refactor: Calculator method early return

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,34 +1,34 @@
 class Calculator {
   sum(num1, num2) {
-    if (this.validate(num1, num2)) {
-      return Math.floor(num1 + num2);
-    } else {
+    if (!this.validate(num1, num2)) {
       throw new Error('숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다!');
     }
+
+    return Math.floor(num1 + num2);
   }
 
   subtract(num1, num2) {
-    if (this.validate(num1, num2)) {
-      return Math.floor(num1 - num2);
-    } else {
+    if (!this.validate(num1, num2)) {
       throw new Error('숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다!');
     }
+
+    return Math.floor(num1 - num2);
   }
 
   multiply(num1, num2) {
-    if (this.validate(num1, num2)) {
-      return Math.floor(num1 * num2);
-    } else {
+    if (!this.validate(num1, num2)) {
       throw new Error('숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다!');
     }
+
+    return Math.floor(num1 * num2);
   }
 
   divide(num1, num2) {
-    if (this.validate(num1, num2)) {
-      return Math.floor(num1 / num2);
-    } else {
+    if (!this.validate(num1, num2)) {
       throw new Error('숫자는 한번에 최대 3자리 수까지만 다룰 수 있습니다!');
     }
+
+    return Math.floor(num1 / num2);
   }
 
   validate(num1, num2) {

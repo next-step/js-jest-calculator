72433f09d067f635a6b02c7c99025ea5d5646ff9
yujeong <yujeong.choung@gmail.com>
2023-07-24 23:15:28 +0900

    test: 숫자 범위 테스트케이스 추가

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,4 +1,5 @@
 import { Calculator } from "../src";
+import { ERROR_MESSAGE } from "../src/constants";
 
 const calculator = new Calculator();
 
@@ -47,5 +48,39 @@ describe("2개의 숫자에 대해 나눗셈이 가능하다.", () => {
   });
   test("1과 1의 나눗셈은 1이다.", () => {
     expect(calculator.divide(1, 1)).toBe(1);
+  });
+});
+
+describe("숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.", () => {
+  it("3자리 수를 벗어나면 에러가 발생한다.", () => {
+    try {
+      calculator.sum(1000, 2);
+    } catch (error) {
+      expect(error.message).toBe(ERROR_MESSAGE.NUMBER_SCOPE);
+    }
+  });
+
+  it("3자리 수를 벗어나면 에러가 발생한다.", () => {
+    try {
+      calculator.subtract(0, -1000);
+    } catch (error) {
+      expect(error.message).toBe(ERROR_MESSAGE.NUMBER_SCOPE);
+    }
+  });
+
+  it("3자리 수를 벗어나면 에러가 발생한다.", () => {
+    try {
+      calculator.divide(0, -1000);
+    } catch (error) {
+      expect(error.message).toBe(ERROR_MESSAGE.NUMBER_SCOPE);
+    }
+  });
+
+  it("3자리 수를 벗어나지 않으면 에러가 발생하지 않는다.", () => {
+    try {
+      expect(calculator.multiply(0, 100)).toBe(0);
+    } catch (error) {
+      expect(error.message).toBe(ERROR_MESSAGE.NUMBER_SCOPE);
+    }
   });
 });

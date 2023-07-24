2ec7c2c744539c889a640a987e27d6b9e8a3e883
yujeong <yujeong.choung@gmail.com>
2023-07-24 20:08:53 +0900

    teat: sum에 대한 테스트 케이스 생성

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,3 +1,15 @@
 import { Calculator } from "../src";
 
 const calculator = new Calculator();
+
+describe("2개의 숫자에 대해 덧셈이 가능하다.", () => {
+  test("3.2와 2의 합은 5이다.", () => {
+    expect(calculator.sum(3.2, 2)).toBe(5);
+  });
+  test("-1과 1의 합은 0이다.", () => {
+    expect(calculator.sum(-1, 1)).toBe(0);
+  });
+  test("1과 1의 합은 2이다.", () => {
+    expect(calculator.sum(1, 1)).toBe(2);
+  });
+});

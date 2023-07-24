29d6ce22154b8304f23a0880513a5ff60474c0d6
yujeong <yujeong.choung@gmail.com>
2023-07-24 20:13:00 +0900

    test: 뺄셈에 대한 테스트 케이스 생성

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -13,3 +13,15 @@ describe("2개의 숫자에 대해 덧셈이 가능하다.", () => {
     expect(calculator.sum(1, 1)).toBe(2);
   });
 });
+
+describe("2개의 숫자에 대해 뺄셈이 가능하다.", () => {
+  test("3.2와 2의 차는 1이다.", () => {
+    expect(calculator.subtract(3.2, 2)).toBe(1);
+  });
+  test("-1과 1의 차는 -2이다.", () => {
+    expect(calculator.sum(-1, 1)).toBe(-2);
+  });
+  test("1과 1의 차는 0이다.", () => {
+    expect(calculator.sum(1, 1)).toBe(0);
+  });
+});

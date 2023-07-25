9bffe80ca6716954445cd006af58e2edab717299
ding-co <pangoons@naver.com>
2023-07-25 13:14:43 +0900

    refactor: Add validate method to throw error for reducing code

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,33 +4,25 @@ class Calculator {
   #MAX_DIGITS = 1_000;
 
   sum(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Math.floor(num1 + num2);
   }
 
   subtract(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Math.floor(num1 - num2);
   }
 
   multiply(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Math.floor(num1 * num2);
   }
 
   divide(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     if (num2 === 0) {
       throw CustomError.DIVISION;
@@ -42,33 +34,25 @@ class Calculator {
   // Float operation toFixed(2) b/c Not Exact operation for computer
 
   fsum(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Number((num1 + num2).toFixed(2));
   }
 
   fsubtract(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Number((num1 - num2).toFixed(2));
   }
 
   fmultiply(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     return Number((num1 * num2).toFixed(2));
   }
 
   fdivide(num1, num2) {
-    if (!this.isValid(num1, num2)) {
-      throw CustomError.DIGITS;
-    }
+    this.validate(num1, num2);
 
     if (num2 === 0) {
       throw CustomError.DIVISION;
@@ -79,6 +63,12 @@ class Calculator {
 
   isValid(num1, num2) {
     return num1 < this.#MAX_DIGITS && num2 < this.#MAX_DIGITS;
+  }
+
+  validate(num1, num2) {
+    if (!this.isValid(num1, num2)) {
+      throw CustomError.DIGITS;
+    }
   }
 }
 

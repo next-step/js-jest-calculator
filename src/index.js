07e831d3780636fa69365e8b0c4687b2e49ffc64
양효령 <js980303@naver.com>
2023-07-25 09:04:07 +0900

    feat: 2개의 함수에 대한 divide method 작성

--- src/index.js
+++ src/index.js
@@ -10,4 +10,11 @@ class Calculator {
   multiply(num1, num2) {
     return Math.floor(num1 * num2);
   }
+
+  divide(num1, num2) {
+    if (num2 === 0) {
+      return new Error("0으로 나눌 수 없습니다.");
+    }
+    return Math.floor(num1 / num2);
+  }
 }

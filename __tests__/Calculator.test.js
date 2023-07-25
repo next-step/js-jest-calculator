877ac7d2fed7861402f35e66210b53f54d38bf83
ding-co <pangoons@naver.com>
2023-07-25 12:17:19 +0900

    feat: Add Calculator floating point operation methods, test cases

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -29,8 +29,6 @@ describe('Calculator', () => {
     expect(calculator.sum(-10, +20)).toBe(10);
   });
 
-  // 2개의 소수에 대해 소숫점 연산이 가능하다.
-
   // 2개의 숫자에 대해 뺄셈이 가능하다.
   it('Subtracts 1 - 2 to equal -1', () => {
     expect(calculator.subtract(1, 2)).toBe(-1);
@@ -47,6 +45,29 @@ describe('Calculator', () => {
   });
   it('Divides 4 / 0 to Error', () => {
     expect(() => calculator.divide(4, 0)).toThrow(CustomError.DIVISION);
+  });
+
+  // 2개의 부동 소수점 수에 대해 덧셈이 가능하다.
+  it('Adds 1.4 + 2.3 to equal 3.7', () => {
+    expect(calculator.fsum(1.4, 2.3)).toBe(3.7);
+  });
+
+  // 2개의 부동 소수점 수에 대해 뺄셈이 가능하다.
+  it('Subtracts 1.5 - 2.4 to equal -0.9', () => {
+    expect(calculator.fsubtract(1.5, 2.4)).toBe(-0.9);
+  });
+
+  // 2개의 부동 소수점 수에 대해 곱셈이 가능하다.
+  it('Multiply 1.2 * 2.0 to equal 2.4', () => {
+    expect(calculator.fmultiply(1.2, 2.0)).toBe(2.4);
+  });
+
+  // 2개의 부동 소수점 수에 대해 나눗셈이 가능하다.
+  it('Divides 4.2 / 2.0 to equal 2.1', () => {
+    expect(calculator.fdivide(4.2, 2.0)).toBe(2.1);
+  });
+  it('Divides 4.5 / 0.0 to Error', () => {
+    expect(() => calculator.fdivide(4.5, 0.0)).toThrow(CustomError.DIVISION);
   });
 
   // 숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.
--- src/Calculator.js
+++ src/Calculator.js
@@ -39,6 +39,44 @@ class Calculator {
     return Math.floor(num1 / num2);
   }
 
+  // Float operation toFixed(2) b/c Not Exact operation for computer
+
+  fsum(num1, num2) {
+    if (!this.validate(num1, num2)) {
+      throw CustomError.DIGITS;
+    }
+
+    return Number((num1 + num2).toFixed(2));
+  }
+
+  fsubtract(num1, num2) {
+    if (!this.validate(num1, num2)) {
+      throw CustomError.DIGITS;
+    }
+
+    return Number((num1 - num2).toFixed(2));
+  }
+
+  fmultiply(num1, num2) {
+    if (!this.validate(num1, num2)) {
+      throw CustomError.DIGITS;
+    }
+
+    return Number((num1 * num2).toFixed(2));
+  }
+
+  fdivide(num1, num2) {
+    if (!this.validate(num1, num2)) {
+      throw CustomError.DIGITS;
+    }
+
+    if (num2 === 0) {
+      throw CustomError.DIVISION;
+    }
+
+    return Number((num1 / num2).toFixed(2));
+  }
+
   validate(num1, num2) {
     return num1 < this.#MAX_DIGITS && num2 < this.#MAX_DIGITS;
   }

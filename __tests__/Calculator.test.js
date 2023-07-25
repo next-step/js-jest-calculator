bb0f4b7e1a36909cbb3e6983fee0e8532d7e5589
양효령 <js980303@naver.com>
2023-07-25 09:53:19 +0900

    test: '2개의 숫자에 대해 뻴셈이 가능하다' 테스트 작성

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -13,4 +13,15 @@ describe("Calculator", () => {
     expect(calculator.add("abc", 2)).toEqual(Calculator.ERRORS.NOT_A_NUMBER);
     expect(calculator.add(1000, 2)).toEqual(Calculator.ERRORS.INVALID_INPUT);
   });
+
+  test("2개의 숫자에 대해 뺄셈이 가능하다", () => {
+    expect(calculator.subtract(2, 1)).toBe(1);
+    expect(calculator.subtract("200", "100")).toBe(100);
+    expect(calculator.subtract("abc", 2)).toEqual(
+      Calculator.ERRORS.NOT_A_NUMBER
+    );
+    expect(calculator.subtract(1000, 2)).toEqual(
+      Calculator.ERRORS.INVALID_INPUT
+    );
+  });
 });

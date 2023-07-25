a99c273bc9ad0c109e5cfeb69356ccf085bdbae8
양효령 <js980303@naver.com>
2023-07-25 09:09:35 +0900

    feat: 숫자 입력 범위 validate private method 작성

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 class Calculator {
   static ERRORS = {
     DIVIDE_BY_ZERO: { errorMessage: "0으로 나눌 수 없습니다" },
+    INVALID_INPUT: {
+      errorMessage: "한번에 최대 3자리 수까지 입력할 수 있습니다",
+    },
   };
+
+  #validateNumberRange(num) {
+    if (num < 0 || num > 999) {
+      return Calculator.ERRORS.INVALID_INPUT;
+    }
+    return num;
+  }
 
   add(num1, num2) {
     return Math.floor(num1 + num2);

a6039b089fb95b43597547128a3c30a679b1a23c
Youngchai Kim <yckim1224@kigam.re.kr>
2023-07-25 09:15:53 +0900

    Add more test cases

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -1,23 +1,66 @@
 import { describe, expect, test } from "@jest/globals";
 
-import { Calculator } from "../src";
+import { Calculator, InvalidOperandError } from "../src";
 
 describe("Calculator", () => {
   const calculator = new Calculator();
 
-  test("1 plus 2 should equal 3", () => {
-    expect(calculator.add(1, 2)).toBe(3);
+  // Good cases
+  test.each([
+    [1, 2, 3],
+    [11, 22, 33],
+    [888, 888, 1776],
+    [0.5, 0.5, 1],
+    [0.5, 0.9, 1],
+  ])("add", (param1, param2, expected) => {
+    expect(calculator.add(param1, param2)).toEqual(expected);
   });
 
-  test("2 minus 1 should be 1", () => {
-    expect(calculator.subtract(2, 1)).toBe(1);
+  test.each([
+    [2, 1, 1],
+    [22, 11, 11],
+    [222, 111, 111],
+    [0.5, 0.5, 0],
+    [1.5, 0.5, 1],
+  ])("subtract", (param1, param2, expected) => {
+    expect(calculator.subtract(param1, param2)).toEqual(expected);
   });
 
-  test("3 times 4 should be 12", () => {
-    expect(calculator.multiply(3, 4)).toBe(12);
+  test.each([
+    [2, 1, 2],
+    [22, 11, 242],
+    [222, 111, 24642],
+    [0.5, 0.5, 0],
+    [1.5, 1.5, 2],
+  ])("multifly", (param1, param2, expected) => {
+    expect(calculator.multiply(param1, param2)).toEqual(expected);
   });
 
-  test("4 division by 2 should be 2", () => {
-    expect(calculator.divide(4, 2)).toBe(2);
+  test.each([
+    [2, 1, 2],
+    [22, 11, 2],
+    [222, 111, 2],
+    [0.5, 0.5, 1],
+    [1, 1.5, 0],
+  ])("division", (param1, param2, expected) => {
+    expect(calculator.divide(param1, param2)).toBe(expected);
+  });
+
+  // Bad cases
+  test.each([
+    [1234, 0],
+    [0, 1234],
+    [1234, 5678],
+  ])("throws InvalidOperandError", (param1, param2) => {
+    expect(() => calculator.add(param1, param2)).toThrow(InvalidOperandError);
+    expect(() => calculator.subtract(param1, param2)).toThrow(
+      InvalidOperandError
+    );
+    expect(() => calculator.multiply(param1, param2)).toThrow(
+      InvalidOperandError
+    );
+    expect(() => calculator.divide(param1, param2)).toThrow(
+      InvalidOperandError
+    );
   });
 });

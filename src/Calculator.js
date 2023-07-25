0310c88ba860f8bb86f569ee19397529ead095c2
y00n-lee <0613myday@gmail.com>
2023-07-25 22:17:17 +0900

    feat] 뺄셈 기능 구현

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,16 +1,33 @@
 export const sum = (nList) => {
-  inputNumTypeCheck(nList);
-  return validateLength(Math.floor(nList.reduce((acc, cur) => acc + cur, 0)));
+  validateInputType(nList);
+  validateInputLength(nList);
+  return validateReturnLength(
+    Math.floor(nList.reduce((acc, cur) => acc + cur, 0))
+  );
 };
 
-const inputNumTypeCheck = (nList) => {
+export const subtract = (nList) => {
+  validateInputType(nList);
+  validateInputLength(nList);
+  return validateReturnLength(
+    Math.floor(nList.reduce((acc, cur) => acc - cur, nList[0] * 2))
+  );
+};
+
+const validateInputType = (nList) => {
   for (const n of nList) {
     if (typeof n !== "number") throw new Error("계산기에 숫자만 입력해주세요.");
   }
 };
 
-const validateLength = (n) => {
-  if (Math.floor(n / 100) >= 10)
-    throw new Error("연산 결과가 세자리 이상입니다.");
+const validateInputLength = (nList) => {
+  for (const n of nList) {
+    if (String(n).length > 3)
+      throw new Error("입력 자리수가 세자리 초과입니다.");
+  }
+};
+
+const validateReturnLength = (n) => {
+  if (String(n).length > 3) throw new Error("연산 결과가 세자리 초과입니다.");
   return n;
 };

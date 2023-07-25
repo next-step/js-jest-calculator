dfc079ede43e2be6b7e5685257d9047de1136ddb
ericagong <48196721+ericagong@users.noreply.github.com>
2023-07-25 14:06:00 +0900

    feat: [feature1] 작동하는 코드 구현 및 리팩토링

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -21,61 +21,61 @@ describe("피연산자가 유효한지 검사한다.", () => {
   const long_float_neg_operand = -1234.456;
 
   it("피연산자가 빈 값이면, 오류 메시지를 반환한다.", () => {
-    expect(() => calculator.validate(empty_operand, valid_operand)).toThrow(
-      ERROR_MESSAGE.EMPTY_OPERAND
-    );
+    expect(() =>
+      calculator._validate_operands(empty_operand, valid_operand)
+    ).toThrow(ERROR_MESSAGE.EMPTY_OPERAND);
   });
 
   it("피연산자가 숫자나 문자열 형태의 숫자가 아니라면, 오류 메시지를 반환한다.", () => {
-    expect(() => calculator.validate(string_operand, valid_operand)).toThrow(
-      ERROR_MESSAGE.INVALID_OPERAND
-    );
+    expect(() =>
+      calculator._validate_operands(string_operand, valid_operand)
+    ).toThrow(ERROR_MESSAGE.INVALID_OPERAND);
 
     // 숫자로 변환 가능한 올바른 형태의 문자열이라면, 오류 메시지를 반환하지 않는다.
     expect(() =>
-      calculator.validate(String(int_pos_operand), valid_operand)
+      calculator._validate_operands(String(int_pos_operand), valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(String(int_neg_operand), valid_operand)
+      calculator._validate_operands(String(int_neg_operand), valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(String(float_pos_operand), valid_operand)
+      calculator._validate_operands(String(float_pos_operand), valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(String(float_neg_operand), valid_operand)
+      calculator._validate_operands(String(float_neg_operand), valid_operand)
     ).not.toThrow();
   });
 
   it("피연산자가 세 자리 초과라면, 오류 메시지를 반환한다.", () => {
     expect(() =>
-      calculator.validate(long_int_pos_operand, valid_operand)
+      calculator._validate_operands(long_int_pos_operand, valid_operand)
     ).toThrow(ERROR_MESSAGE.LONG_OPERAND);
     expect(() =>
-      calculator.validate(long_int_neg_operand, valid_operand)
+      calculator._validate_operands(long_int_neg_operand, valid_operand)
     ).toThrow(ERROR_MESSAGE.LONG_OPERAND);
     expect(() =>
-      calculator.validate(long_float_pos_operand, valid_operand)
+      calculator._validate_operands(long_float_pos_operand, valid_operand)
     ).toThrow(ERROR_MESSAGE.LONG_OPERAND);
     expect(() =>
-      calculator.validate(long_float_neg_operand, valid_operand)
+      calculator._validate_operands(long_float_neg_operand, valid_operand)
     ).toThrow(ERROR_MESSAGE.LONG_OPERAND);
   });
 
   it("그 외의 경우에는 올바른 피연산자로 판단한다.", () => {
     expect(() =>
-      calculator.validate(valid_operand, valid_operand)
+      calculator._validate_operands(valid_operand, valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(int_pos_operand, valid_operand)
+      calculator._validate_operands(int_pos_operand, valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(int_neg_operand, valid_operand)
+      calculator._validate_operands(int_neg_operand, valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(float_pos_operand, valid_operand)
+      calculator._validate_operands(float_pos_operand, valid_operand)
     ).not.toThrow();
     expect(() =>
-      calculator.validate(float_neg_operand, valid_operand)
+      calculator._validate_operands(float_neg_operand, valid_operand)
     ).not.toThrow();
   });
 });
@@ -87,61 +87,66 @@ describe("피연산자 두 개와 연산자 하나의 연산 결과를 반환한
   const neg_operand = -31;
 
   it("두 개의 피연산자에 대해 덧셈 결과를 반환한다.", () => {
-    expect(calculator.add(pos_operand, neg_operand)).toBe(
+    expect(calculator._add(pos_operand, neg_operand)).toBe(
       pos_operand + neg_operand
     );
   });
 
   it("두 개의 피연산자에 대해 뺄셈 결과를 반환한다.", () => {
-    expect(calculator.subtarct(pos_operand, neg_operand)).toBe(
+    expect(calculator._subtract(pos_operand, neg_operand)).toBe(
       pos_operand - neg_operand
     );
   });
 
   it("두 개의 피연산자에 대해 곱셈 결과를 반환한다.", () => {
-    expect(calculator.multiply(pos_operand, neg_operand)).toBe(
+    expect(calculator._multiply(pos_operand, neg_operand)).toBe(
       pos_operand * neg_operand
     );
   });
 
   it("두 개의 피연산자에 대해 나눗셈 결과를 반환한다.", () => {
-    expect(calculator.divide(pos_operand, neg_operand)).toBe(
+    expect(calculator._divide(pos_operand, neg_operand)).toBe(
       pos_operand / neg_operand
     );
   });
 
   it("첫 번째 피연산자는 0이 아니고, 두 번쨰 피연산자가 +0/-0인 경우, 나눗셈 결과 (첫번째 피연산자 부호 * 두 번째 피연산자 부호) * Infinity를 반환한다.", () => {
-    expect(calculator.divide(pos_operand, POS_ZERO)).toBe(POS_INF);
-    expect(calculator.divide(pos_operand, NEG_ZERO)).toBe(NEG_INF);
-    expect(calculator.divide(neg_operand, POS_ZERO)).toBe(NEG_INF);
-    expect(calculator.divide(neg_operand, NEG_ZERO)).toBe(POS_INF);
+    expect(calculator._divide(pos_operand, POS_ZERO)).toBe(POS_INF);
+    expect(calculator._divide(pos_operand, NEG_ZERO)).toBe(NEG_INF);
+    expect(calculator._divide(neg_operand, POS_ZERO)).toBe(NEG_INF);
+    expect(calculator._divide(neg_operand, NEG_ZERO)).toBe(POS_INF);
   });
 
   it("두 피연산자가 모두 0일 때, 나눗셈 결과 NaN을 반환한다.", () => {
-    expect(calculator.divide(POS_ZERO, POS_ZERO)).toBeNaN();
-    expect(calculator.divide(POS_ZERO, NEG_ZERO)).toBeNaN();
-    expect(calculator.divide(NEG_ZERO, POS_ZERO)).toBeNaN();
-    expect(calculator.divide(NEG_ZERO, NEG_ZERO)).toBeNaN();
+    expect(calculator._divide(POS_ZERO, POS_ZERO)).toBeNaN();
+    expect(calculator._divide(POS_ZERO, NEG_ZERO)).toBeNaN();
+    expect(calculator._divide(NEG_ZERO, POS_ZERO)).toBeNaN();
+    expect(calculator._divide(NEG_ZERO, NEG_ZERO)).toBeNaN();
   });
 });
 
-// feature 3 : 사칙 연산 결과 디스플레이
-describe("화면에 출력할 연산 결과를 반환한다.", () => {
+// feature 3 : 사칙 연산 결과 유효성 검사 및 변환
+describe("유효하지 않은 연산 결과가 나오면, 오류 처리한다.", () => {
   it("연산 결과 NaN인 경우, 오류 메시지를 반환한다.", () => {
-    expect(() => calculator.display(NaN)).toThrow(ERROR_MESSAGE.INVALID_RESULT);
+    expect(() => calculator._validate_ouptut(NaN)).toThrow(
+      ERROR_MESSAGE.INVALID_RESULT
+    );
   });
 
   it("연산 결과 -Infinity/Infinity인 경우, 오류 메시지를 반환한다.", () => {
-    expect(() => calculator.display(NEG_INF)).toThrow(
+    expect(() => calculator._validate_ouptut(NEG_INF)).toThrow(
       ERROR_MESSAGE.INVALID_RESULT
     );
-    expect(() => calculator.display(POS_INF)).toThrow(
+    expect(() => calculator._validate_ouptut(POS_INF)).toThrow(
       ERROR_MESSAGE.INVALID_RESULT
     );
   });
+});
 
+// feature 4 : 유효한 사칙 연산 결과를 조정하여 출력
+describe("사칙연산 결과를 조정하여 출력환다.", () => {
   it("연산 결과가 -0인 경우, 0을 반환한다.", () => {
-    expect(calculator.display(NEG_ZERO)).toBe(POS_ZERO);
+    expect(calculator._adjust_output(NEG_ZERO)).toBe(POS_ZERO);
   });
 
   it("그 외의 경우, 연산 결과의 소수점은 버림 처리하고 반환한다.", () => {
@@ -150,13 +155,17 @@ describe("화면에 출력할 연산 결과를 반환한다.", () => {
     const pos_float = 1995.1031;
     const neg_float = -1995.1031;
 
-    expect(calculator.display(pos_int)).toBe(pos_int.toFixed(DECIMAL_PLACE));
-    expect(calculator.display(neg_int)).toBe(neg_int.toFixed(DECIMAL_PLACE));
+    expect(calculator._adjust_output(pos_int)).toBe(
+      pos_int.toFixed(DECIMAL_PLACE)
+    );
+    expect(calculator._adjust_output(neg_int)).toBe(
+      neg_int.toFixed(DECIMAL_PLACE)
+    );
 
-    expect(calculator.display(pos_float)).toBe(
+    expect(calculator._adjust_output(pos_float)).toBe(
       pos_float.toFixed(DECIMAL_PLACE)
     );
-    expect(calculator.display(neg_float)).toBe(
+    expect(calculator._adjust_output(neg_float)).toBe(
       neg_float.toFixed(DECIMAL_PLACE)
     );
   });
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,33 +2,91 @@ import { POS_ZERO, NEG_ZERO, POS_INF, NEG_INF } from "./constants/numbers";
 import { DECIMAL_PLACE, ERROR_MESSAGE } from "./constants/settings";
 
 export default class Calculator {
-  validate(operand1, operand2) {}
+  _get_digits(operand) {
+    return parseInt(Math.abs(operand)).toString().length;
+  }
 
-  add(operand1, operand2) {
+  _validate_operands(operand1, operand2) {
+    if (operand1 === "" || operand2 === "") {
+      throw new Error(ERROR_MESSAGE.EMPTY_OPERAND);
+    }
+
+    if (isNaN(operand1) || isNaN(operand2)) {
+      throw new Error(ERROR_MESSAGE.INVALID_OPERAND);
+    }
+
+    if (
+      this._get_digits(operand1) > DECIMAL_PLACE ||
+      this._get_digits(operand2) > DECIMAL_PLACE
+    ) {
+      throw new Error(ERROR_MESSAGE.LONG_OPERAND);
+    }
+  }
+
+  _add(operand1, operand2) {
     return operand1 + operand2;
   }
 
-  subtarct(operand1, operand2) {
+  _subtract(operand1, operand2) {
     return operand1 - operand2;
   }
 
-  multiply(operand1, operand2) {
+  _multiply(operand1, operand2) {
     return operand1 * operand2;
   }
 
-  divide(operand1, operand2) {
+  _divide(operand1, operand2) {
     return operand1 / operand2;
   }
 
-  display(result) {
-    if (isNaN(result) || result === POS_INF || result === NEG_INF) {
+  _validate_ouptut(result) {
+    if (Number.isNaN(result) || result === POS_INF || result === NEG_INF) {
       throw new Error(ERROR_MESSAGE.INVALID_RESULT);
     }
 
+    return;
+  }
+
+  _adjust_output(result) {
     if (Object.is(result, NEG_ZERO)) {
       return POS_ZERO;
     }
 
     return result.toFixed(DECIMAL_PLACE);
+  }
+
+  _display(result) {
+    console.log(result);
+  }
+
+  calculate(operand1, operand2, operator) {
+    try {
+      let result = 0;
+
+      this._validate_operands(operand1, operand2);
+
+      switch (operator) {
+        case "+":
+          result = this._add(operand1, operand2);
+          break;
+        case "-":
+          result = this._subtract(operand1, operand2);
+          break;
+        case "*":
+          result = this._multiply(operand1, operand2);
+          break;
+        case "/":
+          result = this._divide(operand1, operand2);
+          break;
+        default:
+          throw new Error(ERROR_MESSAGE.INVALID_OPERATOR);
+      }
+
+      this._validate_output(result);
+
+      this._display(this._adjust_output(result));
+    } catch (error) {
+      this._display(error.message);
+    }
   }
 }

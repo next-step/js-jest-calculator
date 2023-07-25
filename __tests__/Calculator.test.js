134fc3a975113cee1f6ce96e5527e4e4bae88907
ding-co <pangoons@naver.com>
2023-07-25 12:11:03 +0900

    feat: Add Calculator test 4 cases for negative/positive number

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -9,6 +9,28 @@ describe('Calculator', () => {
     expect(calculator.sum(1, 2)).toBe(3);
   });
 
+  // 2개의 양수에 대해 덧셈이 가능하다.
+  it('Adds (+3) + (+5) to equal 8', () => {
+    expect(calculator.sum(+3, +5)).toBe(8);
+  });
+
+  // 2개의 음수에 대해 덧셈이 가능하다.
+  it('Adds (-4) + (-6) to equal -10', () => {
+    expect(calculator.sum(-4, -6)).toBe(-10);
+  });
+
+  // 1개의 양수와 1개의 음수에 대해 덧셈이 가능하다.
+  it('Adds (+3) + (-10) to equal -7', () => {
+    expect(calculator.sum(+3, -10)).toBe(-7);
+  });
+
+  // 1개의 음수와 1개의 양수에 대해 덧셈이 가능하다.
+  it('Adds (-10) + (+20) to equal 10', () => {
+    expect(calculator.sum(-10, +20)).toBe(10);
+  });
+
+  // 2개의 소수에 대해 소숫점 연산이 가능하다.
+
   // 2개의 숫자에 대해 뺄셈이 가능하다.
   it('Subtracts 1 - 2 to equal -1', () => {
     expect(calculator.subtract(1, 2)).toBe(-1);

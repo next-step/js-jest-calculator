783500cb4642a14cd7755def4df11dee741a46c9
장정원 <longthegarden@innodep.com>
2023-08-09 10:53:03 +0900

    refactor: 테스트 맥락에 맞게 구분, 동일한 테스트 반복 시 each 사용

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -7,87 +7,101 @@ beforeEach(() => {
     calculator.clearValue();
 });
 
-describe('2개의 숫자에 대해 덧셈이 가능하다.', () => {
-    test('1 + 1 = 2', () => {
-        calculator.calculate(1, 1, "+");
-        expect(calculator.getValue()).toBe(2);
+describe('✅ 입력 테스트', () => {
+    describe('숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.', () => {
+        test.each([
+            {num1: 1000, num2: 3, operator: "+", errorMsg: ERROR_MSG.OPERAND_MAX_LENGTH},
+            {num1: 23.5, num2: 8.4, operator: "+", errorMsg: ERROR_MSG.OPERAND_MAX_LENGTH},
+        ])("$num1 $operator $num2 = ERROR", ({num1, num2, operator, errorMsg}) => {
+            expect(() => {
+                calculator.calculate(num1, num2, operator);
+            }).toThrow(errorMsg);
+        });
+    });
+
+    describe('피연산자는 숫자만 입력할 수 있다.', () => {
+        test.each([
+            {num1: 'TEST', num2: 3, operator: "+", errorMsg: ERROR_MSG.OPERAND_TYPE},
+        ])("$num1 $operator $num2 = ERROR", ({num1, num2, operator, errorMsg}) => {
+            expect(() => {
+                calculator.calculate(num1, num2, operator);
+            }).toThrow(errorMsg);
+        });
+    });
+
+    describe('연산자는 +, -, *, / 만 입력할 수 있다.', () => {
+        test.each([
+            {num1: 52, num2: 30, operator: "&", errorMsg: ERROR_MSG.OPERATOR_TYPE},
+        ])("$num1 $operator $num2 = ERROR", ({num1, num2, operator, errorMsg}) => {
+            expect(() => {
+                calculator.calculate(num1, num2, operator);
+            }).toThrow(errorMsg);
+        });
+    });
+
+    describe('나눗셈에서 두번째 연산자는 0을 입력할 수 없다.', () => {
+        test.each([
+            {num1: 52, num2: 0, operator: "/", errorMsg: ERROR_MSG.SECOND_OPERAND_NOT_ZERO},
+        ])("$num1 $operator $num2 = ERROR", ({num1, num2, operator, errorMsg}) => {
+            expect(() => {
+                calculator.calculate(num1, num2, operator);
+            }).toThrow(errorMsg);
+        });
+    });
+
+});
+
+describe('✅ 연산 테스트', () => {
+    describe('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+        test.each([
+            {num1: 1, num2: 1, operator: "+", expected: 2},
+            {num1: 71, num2: 22, operator: "+", expected: 93}
+        ])("$num1 $operator $num2 = $expected", ({num1, num2, operator, expected}) => {
+            calculator.calculate(num1, num2, operator);
+            expect(calculator.getValue()).toBe(expected);
+        });
+    });
+
+    describe('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+        test.each([
+            {num1: 100, num2: 5, operator: "-", expected: 95},
+            {num1: 1, num2: 3, operator: "-", expected: -2}
+        ])("$num1 $operator $num2 = $expected", ({num1, num2, operator, expected}) => {
+            calculator.calculate(num1, num2, operator);
+            expect(calculator.getValue()).toBe(expected);
+        });
+    });
+
+    describe('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+        test.each([
+            {num1: 2, num2: 5, operator: "*", expected: 10},
+            {num1: 12, num2: 3, operator: "*", expected: 36}
+        ])("$num1 $operator $num2 = $expected", ({num1, num2, operator, expected}) => {
+            calculator.calculate(num1, num2, operator);
+            expect(calculator.getValue()).toBe(expected);
+        });
+    });
+
+    describe('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+        test.each([
+            {num1: 6, num2: 3, operator: "/", expected: 2},
+            {num1: 12, num2: 3, operator: "/", expected: 4}
+        ])("$num1 $operator $num2 = $expected", ({num1, num2, operator, expected}) => {
+            calculator.calculate(num1, num2, operator);
+            expect(calculator.getValue()).toBe(expected);
+        });
     });
 });
 
-describe('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
-    test('100 - 5 = 95', () => {
-        calculator.calculate(100, 5, "-");
-        expect(calculator.getValue()).toBe(95);
-    });
-    test('1 - 3 = -2', () => {
-        calculator.calculate(1, 3, "-");
-        expect(calculator.getValue()).toBe(-2);
-    });
-});
-
-describe('2개의 숫자에 대해 곱셈이 가능하다.', () => {
-    test('2 * 3 = 6', () => {
-        calculator.calculate(2, 3, "*");
-        expect(calculator.getValue()).toBe(6);
+describe('✅ 결과 출력 테스트', () => {
+    describe('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+        test.each([
+            {num1: 1.1, num2: 2.5, operator: "+", expected: 3},
+            {num1: 13, num2: 4, operator: "/", expected: 3}
+        ])("$num1 $operator $num2 = $expected", ({num1, num2, operator, expected}) => {
+            calculator.calculate(num1, num2, operator);
+            expect(calculator.getValue()).toBe(expected);
+        });
     });
 });
 
-describe('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
-    test('6 / 3 = 2', () => {
-        calculator.calculate(6, 3, "/");
-        expect(calculator.getValue()).toBe(2);
-    });
-});
-
-describe('숫자는 한번에 최대 3자리 수까지만 다룰 수 있다.', () => {
-    test('1000 + 3 = ERROR', () => {
-        expect(() => {
-            calculator.calculate(1000, 3, "+");
-        }).toThrow(ERROR_MSG.OPERAND_MAX_LENGTH);
-    });
-    test('23.5 + 8.4 = ERROR', () => {
-        expect(() => {
-            calculator.calculate(23.5, 8.4, "+");
-        }).toThrow(ERROR_MSG.OPERAND_MAX_LENGTH);
-    });
-});
-
-describe('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
-    test('1.1 + 2.5 = 3', () => {
-        calculator.calculate(1.1, 2.5, "+");
-        expect(calculator.getValue()).toBe(3);
-    });
-    test('13 / 4 = 3', () => {
-        calculator.calculate(13, 4, "/");
-        expect(calculator.getValue()).toBe(3);
-    });
-});
-
-describe('피연산자는 숫자만 입력할 수 있다.', () => {
-    test('TEST + 3 = ERROR', () => {
-        expect(() => {
-            calculator.calculate('TEST', 3, "+");
-        }).toThrow(ERROR_MSG.OPERAND_TYPE);
-    });
-    test('TEST + 3 = ERROR', () => {
-        expect(() => {
-            calculator.calculate([], 3, "+");
-        }).toThrow(ERROR_MSG.OPERAND_TYPE);
-    });
-});
-
-describe('연산자는 +, -, *, / 만 입력할 수 있다.', () => {
-    test('52 & 30 = ERROR', () => {
-        expect(() => {
-            calculator.calculate(52, 30, "&");
-        }).toThrow(ERROR_MSG.OPERATOR_TYPE);
-    });
-});
-
-describe('나눗셈에서 두번째 연산자는 0을 입력할 수 없다.', () => {
-    test('52 / 0 = ERROR', () => {
-        expect(() => {
-            calculator.calculate(52, 0, "/");
-        }).toThrow(ERROR_MSG.SECOND_OPERAND_NOT_ZERO);
-    });
-});
